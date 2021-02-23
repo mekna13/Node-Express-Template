5492c53816eba27290fe80921abbf87f669cbcf4
Meghna Pradhan <meghna.prd@gmail.com>
2021-02-23 18:51:30 +0530

    complete template

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 const path = require('path')
 
-app.get("/",(req,res) => {
-    res.render('home');
-})
 
 //set Template Engine
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
+
+
+app.get("/",(req,res) => {
+    res.render('home');
+})
+
 
 const server = app.listen(PORT, () => {
     console.log(`Starting PORT ${PORT}`)
